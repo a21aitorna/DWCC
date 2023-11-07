b2dba90426c9ee96d6aecad0c282ce71f8c0ec3c
unknown <a21aitorna@iessacnlemente.net>
2023-11-07 20:55:52 +0100

    Ejercicio 3 completo

---
+++ Tarea2/Ej3/desplazamiemtoBits.js
@@ -0,0 +1,27 @@
+/*
+125/8
+Se desplaza 3 bits a la derecha, ya que 2 elevado a 3 es 8
+*/
+const resultado1 = 125>>3;
+console.log(resultado1);
+
+/*
+40*4
+Se desplaza 2 bits a la izquierda, ya que 2 elevado a 2 es 4
+*/
+const resultado2 = 40<<2;
+console.log(resultado2);
+
+/*
+25/2
+Se desplaza 1 bit a la derecha, ya que 2 elevado a 1 es 2
+*/
+const resultado3 = 25>>1;
+console.log(resultado3);
+
+/*
+10*16
+Se desplaza 2 bits a la izquierda, ya que 2 elevado a 4 es 16
+*/
+const resultado4 = 10<<4;
+console.log(resultado4);
