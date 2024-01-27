d5ff65571a2031fff56217061eafbd3693b019df
unknown <a21aitorna@iessacnlemente.net>
2024-01-27 15:09:18 +0100

    Nombre tarea cambiado

---
+++ Tarea4.1/arrays.js
@@ -0,0 +1,69 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var paises = ["Albania", "Alemania", "Andorra", "Armenia", "Austria", "Azerbaiyán", "Bélgica", "Bielorrusia", "Bosnia y Herzegovina", "Bulgaria", "Chipre", "Croacia", "Dinamarca", "Eslovaquia", "Eslovenia",
+    "España", "Estonia", "Finlandia", "Francia", "Georgia", "Grecia", "Hungría", "Irlanda", "Islandia", "Italia", "Kazajistán", "Kosovo", "Letonia", "Liechtenstein", "Lituania", "Luxemburgo", "Moldavia",
+    "Mónaco", "Montenegro", "Noruega", "Países Bajos", "Polonia", "Portugal", "Reino Unido", "República Checa", "Rumanía", "Rusia", "Suecia", "Suiza"];
+    
+function mirarNumeroElementos(arrayPaises) {
+    console.log("El numero de paises en el array es ".concat(arrayPaises.length));
+    console.log(arrayPaises);
+}
+function mostrarPaises(arrayPaises) {
+    for (var i in arrayPaises) {
+        console.log("".concat(arrayPaises[i]));
+    }
+}
+function mostrarPaisesInversa(arrayPaises) {
+    var paisesInversa = __spreadArray([], paises, true).reverse();
+    paisesInversa.forEach(function (pais) {
+        console.log(pais);
+    });
+}
+function anhadirPaisComienzo(arrayPaises) {
+    var paisNuevoInicio = document.getElementById("nuevoPaisInicio").value;
+    arrayPaises.splice(0, 0, paisNuevoInicio);
+    console.log(arrayPaises);
+}
+function anhadirPaisFinal(arrayPaises) {
+    var paisNuevoFinal = document.getElementById("nuevoPaisFinal").value;
+    arrayPaises.splice(arrayPaises.length, 0, paisNuevoFinal);
+    console.log(arrayPaises);
+}
+function eliminarElementoComienzo(arrayPaises) {
+    var paisBorradoComienzo = arrayPaises.shift();
+    console.log("El pais borrado al principio es ".concat(paisBorradoComienzo));
+}
+function eliminarElementoFinal(arrayPaises) {
+    var paisBorradoFinal = arrayPaises.pop();
+    console.log("El pais borrado al final es ".concat(paisBorradoFinal));
+}
+function buscarElemento(arrayPaises) {
+    var indice = parseInt(document.getElementById("indice").value, 10);
+    for (var i = 0; i <= arrayPaises.length; i++) {
+        if (indice === i) {
+            console.log("El elemento en la posici\u00F3n ".concat(i, " es ").concat(arrayPaises[i]));
+        }
+    }
+}
+function buscarPosicion(arrayPaises) {
+    var paisBuscar = document.getElementById("paisBuscar").value;
+    console.log("El pais a buscar es ".concat(paisBuscar));
+    for (var i in arrayPaises) {
+        if (arrayPaises[i] === paisBuscar) {
+            console.log("El pais ".concat(paisBuscar, " esta en la posicion ").concat(i));
+        }
+    }
+}
+function mostrarElementosMedio(arrayPaises) {
+    var primeraPosicion = parseInt(document.getElementById("primeraPosicion").value, 10);
+    var segundaPosicion = parseInt(document.getElementById("segundaPosicion").value, 10);
+    var elementosMedio = arrayPaises.slice(primeraPosicion, segundaPosicion);
+    console.log(elementosMedio);
+}
---
+++ Tarea4.1/arrays.ts
@@ -0,0 +1,74 @@
+const paises: string[] = ["Albania","Alemania","Andorra","Armenia","Austria","Azerbaiyán","Bélgica","Bielorrusia","Bosnia y Herzegovina","Bulgaria","Chipre","Croacia","Dinamarca","Eslovaquia","Eslovenia",
+    "España","Estonia","Finlandia","Francia","Georgia","Grecia","Hungría","Irlanda","Islandia","Italia","Kazajistán","Kosovo","Letonia","Liechtenstein","Lituania","Luxemburgo","Moldavia",
+    "Mónaco","Montenegro","Noruega","Países Bajos","Polonia","Portugal","Reino Unido","República Checa","Rumanía","Rusia","Suecia","Suiza"];
+
+
+function mirarNumeroElementos(arrayPaises:string[]){  
+    console.log(`El numero de paises en el array es ${arrayPaises.length}`);
+    console.log(arrayPaises);
+}
+
+function mostrarPaises(arrayPaises:string[]){
+    for(let i in arrayPaises){
+        console.log(`${arrayPaises[i]}`);
+    }
+}
+
+function mostrarPaisesInversa(arrayPaises:string[]){
+    let paisesInversa: string[] =[...paises].reverse();
+    paisesInversa.forEach(pais => {
+        console.log(pais);
+    });
+}
+
+function anhadirPaisComienzo(arrayPaises:string[]){
+    let paisNuevoInicio: string | null = (<HTMLInputElement>document.getElementById("nuevoPaisInicio")).value;
+    arrayPaises.splice(0,0, paisNuevoInicio);
+    console.log(arrayPaises);
+}
+
+function anhadirPaisFinal(arrayPaises:string[]){
+    let paisNuevoFinal: string|null = (<HTMLInputElement>document.getElementById("nuevoPaisFinal")).value;
+    arrayPaises.splice(arrayPaises.length,0, paisNuevoFinal);
+    console.log(arrayPaises);
+}
+
+function eliminarElementoComienzo(arrayPaises:string[]){
+    let paisBorradoComienzo:string|undefined = arrayPaises.shift(); 
+    console.log(`El pais borrado al principio es ${paisBorradoComienzo}`);
+
+}
+
+function eliminarElementoFinal(arrayPaises:string[]){
+    let paisBorradoFinal: string|undefined= arrayPaises.pop();
+    console.log(`El pais borrado al final es ${paisBorradoFinal}`);
+}
+
+
+function buscarElemento(arrayPaises: string[]) {
+    let indice: number = parseInt((<HTMLInputElement>document.getElementById("indice")).value, 10);
+    for (let i:number=0;i<=arrayPaises.length; i++) {
+        if (indice === i) {
+            console.log(`El elemento en la posición ${i} es ${arrayPaises[i]}`);
+        }
+    }
+}
+
+function buscarPosicion(arrayPaises:string[]){
+    let paisBuscar: string = (<HTMLInputElement>document.getElementById("paisBuscar")).value;
+    console.log(`El pais a buscar es ${paisBuscar}`);
+    for(let i in arrayPaises){
+        if(arrayPaises[i] === paisBuscar){
+            console.log(`El pais ${paisBuscar} esta en la posicion ${i}`);
+        }
+    }
+
+}
+
+function mostrarElementosMedio(arrayPaises:string[]){
+    let primeraPosicion:number = parseInt((<HTMLInputElement>document.getElementById("primeraPosicion")).value,10);
+    let segundaPosicion:number = parseInt((<HTMLInputElement>document.getElementById("segundaPosicion")).value,10);
+
+    let elementosMedio:string[] = arrayPaises.slice(primeraPosicion, segundaPosicion);
+    console.log(elementosMedio);
+}
